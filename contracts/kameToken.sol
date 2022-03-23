47abd16db09d33a818e828a575d87f22023fad23
Remix Workspace <>
2022-03-23 15:06:37 +0900

    0323

---
+++ contracts/kameToken.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.8.7;
+
+contract KameToken{
+    int balance;
+
+    constructor() public{
+        balance=0;
+    }
+
+    function getBalance() view public returns(int){
+        return balance;
+    }
+
+    function depositBalance(int amt) public {
+         balance=balance + amt;
+    }
+    function withdrawBalance(int amt) public {
+         balance=balance - amt;
+    }
+
+
+}
